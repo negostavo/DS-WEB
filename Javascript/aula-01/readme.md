ce8442c83ae20149dc47280c8d0195b41ed2247b
negostavo <dantinhas2008@gmail.com>
2025-01-31 12:26:09 -0300

    primeiro commit

---
+++ Javascript/aula-01/readme.md
@@ -0,0 +1,3 @@
+primeira aula sobre js
+
+aprendemos a fazer somas e usar o prompt do google para atribuir caractedres e ou numeros
---
+++ Javascript/aula-01/script.js
@@ -0,0 +1,25 @@
+console.log("Ola Mundo");
+
+//exemplo de alert
+alert("Ola Mundo 1");
+
+//exemplo de confirm
+var teste = confirm ("Ola Mundo 2");
+    //exibindo a opção escolhida pelo usuario
+    console.log(teste);
+
+//exemplo de prompt()
+var testePrompt = prompt ("digite seu nome");
+    //exibindo nome do usuario
+    console.log(testePrompt);
+
+//tentativa de soma
+     var numero1 = Number.parseInt (prompt("digite o primeiro numero"));
+     var numero2 = Number.parseInt (prompt("digite o segundo numero"));
+     var result = (numero1+numero2);
+     alert(result);
+     //verificando o tipo da variavel
+     console.log(result);
+     console.log(typeof(result));
+
+
